9f6dca9f238f88cc9c8058ae3d63b14cddb4d1ad
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-14 15:27:49 -0400

    Function 2

--- sdi-lab5/script.js
+++ sdi-lab5/script.js
@@ -14,5 +14,37 @@ function addition (num1, num2) {
     return (add);
 }
 
+function subtraction (num1,num2) {
+    var subtract = num1 - num2;
+    return (subtract);
+}
 
+function multiplication (num1, num2) {
+    var multiply = num1 * num2;
+    return (multiply);
+}
+
+function division(num1, num2) {
+    var divide = num1 / num2;
+    return (divide);
+
+}
+//Main Code
+
+if(opPrompt === "+"){
+    final = addition(firstNumber, secondNumber)
+} else if (opPrompt === "-"){
+    final = subtraction(firstNumber, secondNumber)
+} else if (opPrompt === "*") {
+    final = multiplication(firstNumber, secondNumber)
+} else if (opPrompt ==="/") {
+    final = division(firstNumber, secondNumber);
+    if (opPrompt === division && secondNumber === 0){
+        final=console.log("This function is undefined because it cannot be divided by 0.")
+    }
+
+
+}
+
+console.log(firstNumber + opPrompt + secondNumber + " = " + final);
 
