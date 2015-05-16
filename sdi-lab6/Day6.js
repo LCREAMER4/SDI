bb2c09dc4530a8ddf7fb0107fa57689e10907d1f
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-16 14:15:48 -0400

    Working with Arrays Commit 2

--- sdi-lab6/Day6.js
+++ sdi-lab6/Day6.js
@@ -5,13 +5,17 @@ Working With Arrays
  */
 
 function myFunction() {
-
     var petArray = ["Max", "Ollie", "Scruffy", "Streak", "Bambi", "Nugget", "Melody", "Vixie"];
-    var typePetArray =["dog", "dog", "cat", "hamster", "rabbit", "cat", "cat", "cat"];
+    var typePetArray = ["dog", "dog", "cat", "hamster", "rabbit", "cat", "cat", "cat"];
 
     for (var i = 0; i < petArray.length; i++) {
-console.log(petArray[i]+ " is my " + typePetArray[i] + ".");
+        console.log(petArray[i] + " is my " + typePetArray[i] + ".");
+
 
     }
+    petArray.push("Gus");
+    typePetArray.push("dog");
+    console.log(petArray[i] + " is my " + typePetArray[i] + ".");
 }
  myFunction();
+
