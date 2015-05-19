2a62fd85594ec4ffb022725f484619cc13073c45
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-19 10:17:42 -0400

    first commit for even array problem

---
+++ Day7/script.js
@@ -0,0 +1,29 @@
+/**
+ Lara Creamer
+ SDI 1505
+ May 19, 2015
+ */
+
+//global variable
+var numberArray =[1,2,3,4,5,6,7,8,9,10,11,12];
+var returnedArray = [];
+
+
+
+
+
+//function
+function evenArray(argArray) {
+
+
+    //local variables
+    var newArray= [];
+    var modNum;
+
+    console.log(argArray);
+}
+//main code
+returnedArray = evenArray(numberArray);
+
+
+
