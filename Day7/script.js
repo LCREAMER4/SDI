8bf5aec7b3fab7f1b14d927aea428e15463c31a0
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-19 11:02:50 -0400

    commit 3 for numbers array problem

--- Day7/script.js
+++ Day7/script.js
@@ -7,21 +7,37 @@
 //global variable
 var numberArray =[1,2,3,4,5,6,7,8,9,10,11,12];
 var returnedArray = [];
+var userPrompt;
 
+userPrompt = prompt("Enter 'odd' to return odd numbers, or 'even' to return even numbers.");
 
+while (userPrompt != "odd" && userPrompt != "even") {
 
+    userPrompt = prompt ("Hey stupid! Enter 'odd' or 'even'");
+
+}
 
 
 //function
-function evenArray(argArray) {
+function evenArray(argArray, oddOrEven) {
 
 
     //local variables
     var newArray= [];
     var modNum;
+    var zeroOrOne;
+
+    if(oddOrEven === "odd") {
+       zeroOrOne = 1;
+
+    } else {
+        zeroOrOne = 0;
+
+    }
+
     for(i=0; i<argArray.length;i++) {
         modNum = argArray[i] % 2;
-        if(modNum === 1) {
+        if(modNum === zeroOrOne) {
             newArray.push(argArray[i]);
 
         }
@@ -33,8 +49,8 @@ function evenArray(argArray) {
 
 }
 //main code
-returnedArray = evenArray(numberArray);
+returnedArray = evenArray(numberArray, userPrompt);
 
-console.log(" The new numbers array is " + returnedArray);
+console.log(" The " + userPrompt +  " numbers array is " + returnedArray);
 
 
