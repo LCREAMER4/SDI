13b80a2514c1fc5df5f8678f6f4e3205ed5b5ccd
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-19 09:19:17 -0400

    first commit for lab 6 review

---
+++ Day7/day7script.js
@@ -0,0 +1,24 @@
+/**
+ * Created by laracreamer on 5/19/15.
+ */
+
+
+//Lab 6 Review
+
+//function
+function authorFunction(){
+
+    //for local arrays
+
+
+    //for loop to go through the array items
+
+
+    //use an array method to alter the arrays
+
+
+    //do a final output for the change
+
+}
+
+authorFunction();
