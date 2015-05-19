302f216e98df2a5661d4814f3e7eaa8eaf46d001
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-19 09:30:59 -0400

    day6 lab review commit 2

--- Day7/day7script.js
+++ Day7/day7script.js
@@ -9,16 +9,20 @@
 function authorFunction(){
 
     //for local arrays
-
+    var authors = ["Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
+    var titles = ["Oliver Twist", "Hamlet", "Tom Sawyer", "Fear and Loathing in Las Vegas"];
 
     //for loop to go through the array items
-
+for (var i = 0; i < authors.length; i++) {
+    console.log(authors[i] + " wrote " + titles[i] +"." );
+}
 
     //use an array method to alter the arrays
-
+authors.push("Dorathy Allison");
+    titles.push("Bastard Out of Carolina");
 
     //do a final output for the change
-
+console.log("I added " + authors[i] + " who wrote " + titles[i] +".")
 }
 
 authorFunction();
