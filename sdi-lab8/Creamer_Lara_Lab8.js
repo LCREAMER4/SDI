29ff5a5a17954c30f573b79cb37c2c2bd1285f97
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-21 16:11:40 -0400

    commit 4

--- sdi-lab8/Creamer_Lara_Lab8.js
+++ sdi-lab8/Creamer_Lara_Lab8.js
@@ -3,22 +3,37 @@ SDI 1505
 May 21, 2015
 Lab 8
  */
-var myEmail;
-var findAt = "@";
-var findPeriod = ".";
-var noSpaces = " ";
+//var myEmail;
+//var findAt = "@";
+//var findPeriod = ".";
+//var noSpaces = " ";
+//
+//function subString(argEmail) {
+//
+//    if ((argEmail.indexOf(findAt) === -1) || (argEmail.indexOf(findPeriod) === -1) || (argEmail.indexOf(noSpaces) > -1)) {
+//        console.log("Your email is incorrect, please enter a valid email address.");
+//    } else {
+//        console.log("Your email address is correct.")
+//    }
+//
+//}
+//myEmail=prompt("Please enter your email address.");
+//
+//subString(myEmail);
 
-function subString(argEmail) {
+var string1 = "1,2,3,4,5";
+var sep1 = ",";
+var sep2 = "/";
+var newList;
+var currentSpace;
+var holder;
 
-    if ((argEmail.indexOf(findAt) === -1) || (argEmail.indexOf(findPeriod) === -1) || (argEmail.indexOf(noSpaces) > -1)) {
-        console.log("Your email is incorrect, please enter a valid email address.");
-    } else {
-        console.log("Your email address is correct.")
+function stringSplit(string1, sep1, sep2) {
+    newList = string1.remove (","); {
+
+
+
+        return newList
     }
-
 }
-myEmail=prompt("Please enter your email address.");
-
-subString(myEmail);
-
-
+newArray =stringSplit();
