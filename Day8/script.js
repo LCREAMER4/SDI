fb5d17a079ef1f04b46f1f0705d94a87246804b7
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-21 09:19:47 -0400

    commit two

--- Day8/script.js
+++ Day8/script.js
@@ -8,5 +8,18 @@ var array2 = [25, 100, 4, 22, 16, 105, 2, 7];
 var array3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
 
 function bubbleSort(argArray) {
-    
+
+    var num1,
+        num2,
+        holder;
+
+    for(var element in argArray) {
+
+        num1 = argArray[element];
+        num2 = argArray[element+1];
+
+
+    }
 }
+
+bubbleSort(array1);
