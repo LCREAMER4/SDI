73a4b3b95c761b7ed7cf41cdb4d4a46a36e3cd9f
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-21 09:14:51 -0400

    commit 1

---
+++ Day8/script.js
@@ -0,0 +1,12 @@
+/*  Lara Creamer
+SDI 1505
+May 21, 2015
+ */
+
+var array1 =[10, 6, 7, 3, 12, 5];
+var array2 = [25, 100, 4, 22, 16, 105, 2, 7];
+var array3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
+
+function bubbleSort(argArray) {
+    
+}
