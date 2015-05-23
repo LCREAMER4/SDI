3b9dfe5dac515ad951790f0c78a891f84ad77c64
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-23 10:11:51 -0400

    final commit for day 9 review lab

--- Day9/script.js
+++ Day9/script.js
@@ -4,10 +4,25 @@
  Day 9
  */
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 //global variables
 var myEmail = "lcre.amer@fullsail.com";
 var emailTrueOrFalse;
-
+var stringList= "a,c,b,c,d,e+f,g,h";
+var first = ",";
+var second = "/";
 //problem functions
 //First function check an email address
 function checkEmail(argEmail) {
@@ -17,7 +32,7 @@ function checkEmail(argEmail) {
     var noSpaces = argEmail.indexOf (" ");
     var lastAt = argEmail.lastIndexOf("@");
 
-    //check for @ symbol
+  //check for @ symbol
     if(atSymbol === lastAt && atSymbol> -1) {
        goodEmail = true;
     } else {
@@ -37,8 +52,23 @@ function checkEmail(argEmail) {
 return goodEmail
 
 }
+function stringSep(oldString, firstSep, newSep) {
+    var newString = "";
 
+    for (var i = 0; i < oldString.length; i++) {
+
+
+        if (oldString.substr(i, 1) === firstSep) {
+            newString = newString + newSep;
+        } else {
+            newString = newString + oldString.substr(i, 1);
+        }
+    }
+    return newString;
+}
 
 //Main code
-emailTrueOrFalse = checkEmail(myEmail);
-console.log("It is " + emailTrueOrFalse + " that this email is valid");
+//emailTrueOrFalse = checkEmail(myEmail);
+//console.log("It is " + emailTrueOrFalse + " that this email is valid");
+    returnedString = stringSep(stringList, first, second);
+    console.log("The new string is " + returnedString);
