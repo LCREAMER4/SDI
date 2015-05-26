477eb5322c495e3f8677b491db384b815c95e2bc
LCREAMER4 <lcreamer@fullsail.edu>
2015-05-26 15:27:22 -0400

    second commit for lab 10

--- sdi-lab10/script.js
+++ sdi-lab10/script.js
@@ -1,16 +1,42 @@
-function lottery (powerball,flLot){
-    var lot = prompt("Choose either powerball or the Florida Lottery.");
-    var returnedLot;
-    if (lot = "powerball") {
-        returnedLot= Math.round(Math.random(5) * (59 - 1) + 1);
+/*Lara Creamer
+SDI 1505
+Lab 10
+Problem Solving
+ */
+
+
+
+var newArray=[];
+
+var lot = prompt("Choose either powerball or the Florida Lottery(flLot).");
+function lottery (argLottery) {
+    var lot;
+    var poppedItem;
+    var returnedLot = [];
+    if (argLottery === "powerball") {
+        for (var i = 0; i < 5; i++)
+            returnedLot.push(Math.round(Math.random() * (59 - 1) + 1));
     }
-    if(lot = "flLot"){
-        returnedLot= Math.round(Math.random(6)*(53-1) + 1);
+    returnedLot.push(Math.round(Math.random() * (35 - 1) + 1));
 
 
-}
+    if(argLottery === "flLot") {
+        for (var z = 0; z < 6; z++)
+            returnedLot.push(Math.round(Math.random() * (53 - 1) + 1));
+    }
+
 return returnedLot
+
+
 }
+
 //main code
-returnedLot=lottery();
-console.log("Your random numbers for this lottery are" + returnedLot);
+returnedLot=lottery(lot);
+poppedItem = returnedLot.pop();
+if(lot === "powerball") {
+    console.log("Your random numbers for your Powerball lottery are " + returnedLot + " with " + poppedItem + " as the powerball");
+}
+if(lot === "flLot") {
+    console.log("Your random numbers for your Florida lottery are " + returnedLot);
+}
+
